dac64576df3c48db003898e711ca134072472379
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2024-07-17 00:43:22 -0700

    Basic car movement

--- classes/car.js
+++ classes/car.js
@@ -5,7 +5,39 @@ class Car {
     this.width = width;
     this.height = height;
 
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.maxReverseSpeed = 1.5;
+    this.friction = 0.05;
+    this.angle = 0;
+    this.amtAngleAdjustPerFrame = 0.03;
+
     this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) this.speed += this.acceleration;
+    if (this.controls.reverse) this.speed -= this.acceleration;
+
+    const maxReverseSpeed = this.maxReverseSpeed * -1;
+
+    if (this.speed > this.maxSpeed) this.speed = this.maxSpeed;
+    if (this.speed < maxReverseSpeed) this.speed = maxReverseSpeed;
+
+    if (this.speed > 0) this.speed -= this.friction;
+    if (this.speed < 0) this.speed += this.friction;
+
+    if (Math.abs(this.speed) < this.friction) this.speed = 0;
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) this.angle += this.amtAngleAdjustPerFrame * flip;
+      if (this.controls.right) this.angle -= this.amtAngleAdjustPerFrame * flip;
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= this.speed;
   }
 
   /**
@@ -14,13 +46,13 @@ class Car {
    * @returns {void}
    */
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
---
+++ classes/controls.js
@@ -0,0 +1,27 @@
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    this.#addKeyboardListeners();
+  }
+
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      const key = event.key.toLowerCase();
+      if (key === "arrowleft") this.left = true;
+      else if (key === "arrowright") this.right = true;
+      else if (key === "arrowdown") this.reverse = true;
+      else if (key === "arrowup") this.forward = true;
+    };
+    document.onkeyup = (event) => {
+      const key = event.key.toLowerCase();
+      if (key === "arrowleft") this.left = false;
+      else if (key === "arrowright") this.right = false;
+      else if (key === "arrowdown") this.reverse = false;
+      else if (key === "arrowup") this.forward = false;
+    };
+  }
+}
